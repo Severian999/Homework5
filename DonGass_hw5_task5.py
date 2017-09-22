7eb026a7194da8d7c2bf383befec923a2b2afc3f
Severian999 <don.gass@gmail.com>
2017-09-22 00:22:12 -0600

    Update DonGass_hw5_task5.py

--- DonGass_hw5_task5.py
+++ DonGass_hw5_task5.py
@@ -9,11 +9,12 @@ medals = [("ITA","JPN","AUS"), ("KOR","TPE","UKR"), ("KOR","KOR","GBR"),("KOR","
 medal_counts = {}
 
 for gold, silver, bronze in medals:
-    #process gold list
+    #process list of countries
     medal_counts.setdefault(gold, [0,0,0])
     medal_counts.setdefault(silver, [0,0,0])
     medal_counts.setdefault(bronze,[0,0,0])
     
+    #process medal counts
     medal_counts[gold][0] += 1
     medal_counts[silver][1] += 1
     medal_counts[bronze][2] += 1
